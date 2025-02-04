7cfc364bb9712c97987862efa85fbfd540700d69
akameswa <adithya.k.nitt@gmail.com>
2025-02-04 20:34:37 +0000

    hw1: decomment args

--- hw1/q1_q2_classification/train_q1.py
+++ hw1/q1_q2_classification/train_q1.py
@@ -18,16 +18,16 @@ if __name__ == "__main__":
     # You should experiment and choose the correct hyperparameters
     # You should get a map of around 22 in 5 epochs
     ##################################################################
-    # args = ARGS(
-    #     epochs=10,
-    #     inp_size=64,
-    #     use_cuda=True,
-    #     val_every=70
-    #     lr=# TODO,
-    #     batch_size=#TODO,
-    #     step_size=#TODO,
-    #     gamma=#TODO
-    # )
+    args = ARGS(
+        epochs=100,
+        inp_size=64,
+        use_cuda=True,
+        val_every=25,
+        lr=0.01,
+        batch_size=64,
+        step_size=25,
+        gamma=0.5,
+    )
     ##################################################################
     #                          END OF YOUR CODE                      #
     ##################################################################
