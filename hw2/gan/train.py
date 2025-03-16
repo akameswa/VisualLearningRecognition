5072d91700ca2b00ed600940b737efd3ef1df684
akameswa <adithya.k.nitt@gmail.com>
2025-03-16 01:06:23 +0000

    hw2: complete train.py

--- hw2/gan/train.py
+++ hw2/gan/train.py
@@ -28,8 +28,8 @@ def get_optimizers_and_schedulers(gen, disc):
     # The learning rate for the generator should be decayed to 0 over
     # 100K iterations.
     ##################################################################
-    scheduler_discriminator = None
-    scheduler_generator = None
+    scheduler_discriminator = torch.optim.lr_scheduler.LinearLR(optim_discriminator, start_factor=1.0, end_factor=0.0, total_iters=500000)
+    scheduler_generator = torch.optim.lr_scheduler.LinearLR(optim_generator, start_factor=1.0, end_factor=0.0, total_iters=100000)
     ##################################################################
     #                          END OF YOUR CODE                      #
     ##################################################################
@@ -105,8 +105,9 @@ def train_model(
                 # 2. Compute discriminator output on the train batch.
                 # 3. Compute the discriminator output on the generated data.
                 ##################################################################
-                discrim_real = None
-                discrim_fake = None
+                generator_output = gen(train_batch.shape[0])
+                discrim_real = disc(train_batch)
+                discrim_fake = disc(generator_output)
                 ##################################################################
                 #                          END OF YOUR CODE                      #
                 ##################################################################
@@ -115,8 +116,11 @@ def train_model(
                 # TODO 1.5 Compute the interpolated batch and run the
                 # discriminator on it.
                 ###################################################################
-                interp = None
-                discrim_interp = None
+                # https://towardsdatascience.com/demystified-wasserstein-gan-with-gradient-penalty-ba5e9b905ead/
+                eps = torch.randn(train_batch.size(0), 1, 1, 1).to(train_batch.device)
+                eps = eps.expand_as(train_batch)
+                interp = eps * train_batch + (1 - eps) * generator_output
+                discrim_interp = disc(interp)
                 ##################################################################
                 #                          END OF YOUR CODE                      #
                 ##################################################################
@@ -136,8 +140,8 @@ def train_model(
                     # TODO 1.2: Compute generator and discriminator output on
                     # generated data.
                     ###################################################################
-                    fake_batch = None
-                    discrim_fake = None
+                    fake_batch = gen(train_batch.size(0))
+                    discrim_fake = disc(fake_batch)
                     ##################################################################
                     #                          END OF YOUR CODE                      #
                     ##################################################################
@@ -156,7 +160,8 @@ def train_model(
                         # TODO 1.2: Generate samples using the generator.
                         # Make sure they lie in the range [0, 1]!
                         ##################################################################
-                        generated_samples = None
+                        generated_samples = gen(train_batch.size(0))
+                        generated_samples = (generated_samples+1)/2
                         ##################################################################
                         #                          END OF YOUR CODE                      #
                         ##################################################################
