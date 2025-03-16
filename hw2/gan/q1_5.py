d38dada0d2e21e55de666d5c297e2873daafb6ba
akameswa <adithya.k.nitt@gmail.com>
2025-03-16 01:07:44 +0000

    hw2: complete q1_5

--- hw2/gan/q1_5.py
+++ hw2/gan/q1_5.py
@@ -20,7 +20,16 @@ def compute_discriminator_loss(
     # loss_pt2 = lambda * E[(|| grad wrt interpolated_data (D(interpolated_data))|| - 1)^2]
     # loss = loss_pt1 + loss_pt2
     ##################################################################
-    loss = None
+    loss_pt1 = torch.mean(discrim_fake) - torch.mean(discrim_real)
+    grads = torch.autograd.grad(
+        outputs=discrim_interp,
+        inputs=interp,
+        grad_outputs=torch.ones_like(discrim_interp),
+        create_graph=True
+    )[0]
+    grads_norm = grads.view(grads.size(0), -1).norm(2, dim=1)  
+    loss_pt2 = lamb * ((grads_norm - 1)**2).mean()
+    loss = loss_pt1 + loss_pt2
     ##################################################################
     #                          END OF YOUR CODE                      #
     ##################################################################
@@ -32,7 +41,7 @@ def compute_generator_loss(discrim_fake):
     # TODO 1.5: Implement WGAN-GP loss for generator.
     # loss = - E[D(fake_data)]
     ##################################################################
-    loss = None
+    loss = -torch.mean(discrim_fake)
     ##################################################################
     #                          END OF YOUR CODE                      #
     ##################################################################
