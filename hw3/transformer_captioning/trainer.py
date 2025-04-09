a9c302fb634a9cc6d337ab9af636b89735ea9816
akameswa <adithya.k.nitt@gmail.com>
2025-04-09 20:22:41 +0000

    hw3: add cross entropy loss in trainer.py

--- hw3/transformer_captioning/trainer.py
+++ hw3/transformer_captioning/trainer.py
@@ -24,6 +24,9 @@ class Trainer(object):
         #TODO - Compute cross entropy loss between predictions and labels. 
         #Make sure to compute this loss only for indices where label is not the null token.
         #The loss should be averaged over batch and sequence dimensions. 
+        loss = torch.nn.CrossEntropyLoss()
+        index = labels != 0
+        loss = loss(predictions[index], labels[index]).mean()
         return loss
     
     def val(self):
